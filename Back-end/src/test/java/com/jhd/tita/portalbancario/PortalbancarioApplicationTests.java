847bf2bb7f7babf7c3b76e4ac665be7ed96bce60
Johnson Torres <44690752+xJHDx@users.noreply.github.com>
2022-06-16 18:24:00 -0500

    start proyect

---
+++ Back-end/src/test/java/com/jhd/tita/portalbancario/PortalbancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhd.tita.portalbancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalbancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
