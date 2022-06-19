fb1ddb3a41c67dd95405a951e151b60903668d48
Johnson Torres <44690752+xJHDx@users.noreply.github.com>
2022-06-19 09:44:26 -0500

    login

--- Front-end/src/app/views/home/login/login.module.ts
+++ Front-end/src/app/views/home/login/login.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { RegistroComponent } from './registro/registro.component';
 import { IngresoComponent } from './ingreso/ingreso.component';
+import { SplitterModule } from 'primeng/splitter';
+import { SharedModule } from 'primeng/api';
+import { BrowserModule } from '@angular/platform-browser';
+import {PasswordModule} from 'primeng/password';
 
 @NgModule({
-  declarations: [
-    RegistroComponent,
-    IngresoComponent
-  ],
   imports: [
     CommonModule,
-    LoginRoutingModule
-  ]
+    LoginRoutingModule,
+    SharedModule,
+    SplitterModule,
+    BrowserModule,
+    PasswordModule
+  ],
+  declarations: [
+    RegistroComponent,
+    IngresoComponent    
+  ],  
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class LoginModule { }
